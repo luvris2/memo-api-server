dd07ffe1fefb5caea4c450a25d36cad3fb4828fd
luvris2 <luvris2@naver.com>
2022-06-21 17:27:10 +0900

    add

--- ref/mysql_connection.py
+++ ref/mysql_connection.py
@@ -1,6 +1,6 @@
 import mysql.connector
-
 def get_connection() :
+    
     connection = mysql.connector.connect(
         host='yh-db.ccekp8a5rlv0.ap-northeast-2.rds.amazonaws.com',
         database='memo_db',

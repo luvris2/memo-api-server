3b3007bd8629e9a18a67c25c46bf7dbd021de306
EunbyeolKo <105832446+luvris2@users.noreply.github.com>
2022-06-21 17:27:27 +0900

    Delete mysql_connection.py

--- ref/mysql_connection.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-def get_connection() :
-    
-    connection = mysql.connector.connect(
-        host='yh-db.ccekp8a5rlv0.ap-northeast-2.rds.amazonaws.com',
-        database='memo_db',
-        user='memo_db_admin',
-        password='memo_administrator' )
-    return connection
